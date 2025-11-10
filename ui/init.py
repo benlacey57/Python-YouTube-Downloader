18a6656cabf011d3bce74268a3567deae890c1a7
Ben Lacey <85163278+benlacey57@users.noreply.github.com>
2025-11-10 15:04:30 +0000

    Create init.py

---
+++ ui/init.py
@@ -0,0 +1,14 @@
+"""User interface components"""
+from .menu import Menu
+from .settings_menu import SettingsMenu
+from .monitoring_menu import MonitoringMenu
+from .stats_display import StatsDisplay
+from .progress_display import ProgressDisplay
+
+__all__ = [
+    'Menu',
+    'SettingsMenu',
+    'MonitoringMenu',
+    'StatsDisplay',
+    'ProgressDisplay'
+]
